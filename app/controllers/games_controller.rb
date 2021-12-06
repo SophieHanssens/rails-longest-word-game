747d86a684efec0388a837b7e6002961434a3e99
Sophie Hanssens <sophie.hanssens@gmail.com>
2021-12-06 16:09:40 +0100

    test

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,26 @@ class GamesController < ApplicationController
    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
   end
   def score
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    # raise
+
+    # 1) The word can’t be built out of the original grid
+        # Transformer le mot encodé en lettres séparées
+        @words = params[:noun].split("")
+        # Comparer chaque lettre du mot avec chaque lettre de la grille
+        @words.each do |word|
+          if @letters.include?(word)
+            def english_word?(word)
+              response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+              json = JSON.parse(response.read)
+              return json['found']
+            end
+          end
+        end
+    # 2) The word is valid according to the grid, but is not a valid English word
+        # Utiliser l'API
+    # 3) The word is valid according to the grid and is an English word
+
   end
 end
 
